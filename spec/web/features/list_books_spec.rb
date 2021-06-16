ca18c13c68d38207f4fa4f22b4372c507f025a3b
ttrweb <tiffanyttr9671@i.softbank.jp>
2021-06-16 18:04:02 +0900

    fix rspec

--- spec/web/features/list_books_spec.rb
+++ spec/web/features/list_books_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'List books' do
   it 'displays each book on the page' do
     visit '/books'
 
-    within '#books' do
+    within '.books' do
       expect(page).to have_css('.book', count: 2)
     end
   end
