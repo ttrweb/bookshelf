407997440a99a0c85ba06e997aa50ad6928ed20f
ttrweb <tiffanyttr9671@i.softbank.jp>
2021-06-18 17:45:45 +0900

    Displaying Dynamic Data

--- apps/web/controllers/books/index.rb
+++ apps/web/controllers/books/index.rb
@@ -1,10 +1,14 @@
+# apps/web/controllers/books/index.rb
 module Web
   module Controllers
     module Books
       class Index
         include Web::Action
 
+        expose :books
+
         def call(params)
+          @books = BookRepository.new.all
         end
       end
     end
