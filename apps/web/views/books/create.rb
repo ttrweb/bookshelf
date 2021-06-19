3569b131ab7fe88ba45d31d02aa897a91644669a
ttrweb <tiffanyttr9671@i.softbank.jp>
2021-06-19 16:56:34 +0900

    Securing Our Form With Validations

--- apps/web/views/books/create.rb
+++ apps/web/views/books/create.rb
@@ -1,8 +1,10 @@
+# apps/web/views/books/create.rb
 module Web
   module Views
     module Books
       class Create
         include Web::View
+        template 'books/new'
       end
     end
   end
